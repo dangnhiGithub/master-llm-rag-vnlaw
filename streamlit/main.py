d7a93fd3bc5f5e53d6476f66810f60be9edcb6ec
DangNhi-Home <dangnhi2702.contact@gmail.com>
2025-04-01 23:09:23 +0700

    chromadb with trained embedding

--- streamlit/main.py
+++ streamlit/main.py
@@ -7,21 +7,25 @@ st.title("CHATBOT RAG app")
 st.write('Welcome to the CHATBOT RAG app!')
 
 messages = st.container()
-prompt = st.chat_input("Say something")
+
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+st.sidebar.button("Clear Chat History", on_click=lambda: st.session_state.update(messages=[]))
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-st.session_state.messages.append({"role": "user", "content": prompt})
 
-if prompt := st.chat_input("Say something"):
+if prompt:= st.chat_input("Say something"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
     messages.chat_message('user').write(prompt)
     messages.chat_message("agent retriever", avatar="🔍").write("Đây là câu trả lời từ agent retriever")
     messages.chat_message("agent reader", avatar="📚").write("Đây là câu trả lời từ agent reader")
     messages.chat_message("agent generator", avatar="🧠").write("Đây là câu trả lời từ agent generator")
     messages.chat_message("ai").write("Đây là câu trả lời cuối cùng")
+    # Add a button to clear chat history
+    
